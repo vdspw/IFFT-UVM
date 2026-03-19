d52fe1c657da3c512d8c78cd6f682139acf551e4
Vishnu Deep PandurangaRao <143198224+vdspw@users.noreply.github.com>
2026-03-19 10:14:55 -0700

    Enhance sequence_g17 with explanatory comments
    Added comments to clarify the purpose of the sequencer class and its methods.

--- sequence_g17.sv
+++ sequence_g17.sv
@@ -1,5 +1,5 @@
 //This is sequencer class 
-
+// generates input stimulus
 class sequence_g17 extends uvm_sequence#(seqitem_g17);
 
   `uvm_object_utils(sequence_g17)
@@ -11,13 +11,13 @@ class sequence_g17 extends uvm_sequence#(seqitem_g17);
   endfunction
   
   virtual task body();
-	msg = seqitem_g17::type_id::create("msg");
+	  msg = seqitem_g17::type_id::create("msg"); // creating the transaction object
 	
 	repeat(60000) begin
-		start_item(msg);
-		msg.randomize();
+		start_item(msg); // starts transaction creation 
+		msg.randomize(); // randomize
 		$display("----> DataIn = %0h" ,msg.DataIn);
-		finish_item(msg);
+		finish_item(msg); // finish - sent to seqr/driver
 	end
     #13000; //Time delay to adjust the clocks to allow all data to come out
 	`uvm_info("sequence","Sequence Running Successfully", UVM_MEDIUM);
