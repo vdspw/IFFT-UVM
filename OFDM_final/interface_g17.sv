919ce3d85f62f88d3d2e0c45955e676bc61c8796
Vishnu Deep PandurangaRao <143198224+vdspw@users.noreply.github.com>
2026-03-19 10:01:17 -0700

    Improve comments in interface_g17.sv
    Updated comments for clarity in interface_g17.sv

--- OFDM_final/interface_g17.sv
+++ OFDM_final/interface_g17.sv
@@ -1,5 +1,5 @@
 // This is the interface block Which contains the variables 
-
+// creates a bunch of signals , used by driver and monitor from the virtual interface to rd/wr signals to the DUT/
 interface interface_g17(input Clk, input Reset);
 
 	logic Pushin;
